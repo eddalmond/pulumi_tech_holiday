a615803b7dba412e4ac20a48d420d7bb97ef3b60
eddalmond <edd.almond@gmail.com>
2025-10-10 11:15:27 +0100

    eja - another bugfix

--- policies/python/__main__.py
+++ policies/python/__main__.py
@@ -1,3 +1,15 @@
-from .policies import register_policy_pack
+import sys
+from pathlib import Path
 
-register_policy_pack()
+
+def _register() -> None:
+	current_dir = Path(__file__).parent
+	sys.path.insert(0, str(current_dir))
+
+	from policies import register_policy_pack  # type: ignore import
+
+	register_policy_pack()
+
+
+if __name__ == "__main__":
+	_register()
